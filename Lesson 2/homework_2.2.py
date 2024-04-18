fb192e30212d1f763b453d448338ab0cc50e8654
MagestiUA <nikolaiq3@gmail.com>
2024-04-18 20:22:58 +0300

    Homework 2.2

---
+++ Lesson 2/homework_2.2.py
@@ -0,0 +1,10 @@
+numb = int(input('Type a five-digit number and press Enter\n'))
+sumb_1 = (numb // 1) % 10
+sumb_2 = (numb // 10) % 10
+sumb_3 = (numb // 100) % 10
+sumb_4 = (numb // 1000) % 10
+sumb_5 = numb // 10000
+print(f'{sumb_1}{sumb_2}{sumb_3}{sumb_4}{sumb_5}')
+
+# строки не використав
+
