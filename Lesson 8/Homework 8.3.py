fc3bece1f153820bb5e741f0fc531b32e47dbb08
MagestiUA <nikolaiq3@gmail.com>
2024-05-10 15:59:47 +0300

    Homework 8

--- Lesson 8/Homework 8.3.py
+++ Lesson 8/Homework 8.3.py
@@ -19,5 +19,6 @@ if __name__ == '__main__':
 	assert find_unique_value([1, 2, 1, 1]) == 2
 	assert find_unique_value([2, 3, 3, 3, 5, 5]) == 2
 	assert find_unique_value([5, 5, 5, 2, 2, 0.5]) == 0.5
+	assert find_unique_value([5, 5, 5, 2, 2, 2]) is None
 	print("ОК")
 	
