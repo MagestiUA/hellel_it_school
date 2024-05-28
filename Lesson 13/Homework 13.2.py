2b40d40675c6cbde6291a6e5b65657a2b10136dc
MagestiUA <nikolaiq3@gmail.com>
2024-05-28 21:15:27 +0300

    Homework 13.2

--- Lesson 13/Homework 13.2.py
+++ Lesson 13/Homework 13.2.py
@@ -1,5 +1,4 @@
 class Counter:
-
 	def __init__(self, current=1, min_value=0, max_value=10):
 		self.current = current
 		self.min_value = min_value
@@ -9,39 +8,51 @@ class Counter:
 		self.current = start
 
 	def set_max(self, max_max):
-		pass
+		self.max_value = max_max
 
 	def set_min(self, min_min):
-		pass
+		self.min_value = min_min
 
 	def step_up(self):
-		pass
+		if self.current < self.max_value:
+			self.current += 1
+		else:
+			raise ValueError('Достигнут максимум')
 
 	def step_down(self):
-		pass
+		if self.current > self.min_value:
+			self.current -= 1
+		else:
+			raise ValueError('Достигнут минимум')
 
 	def get_current(self):
 		return self.current
 
-counter = Counter()
-counter.set_current(7)
-counter.step_up()
-counter.step_up()
-counter.step_up()
-assert counter.get_current() == 10, 'Test1'
-try:
-	counter.step_up()  # ValueError
-except ValueError as e:
-	print(e) # Достигнут максимум
-assert counter.get_current() == 10, 'Test2'
 
-counter.set_min(7)
-counter.step_down()
-counter.step_down()
-counter.step_down()
-assert counter.get_current() == 7, 'Test3'
-try:
-	counter.step_down()  # ValueError
-except ValueError as e:
-	print(e) # Достигнут минимум
-assert counter.get_current() == 7, 'Test4'
+if __name__ == '__main__':
+	counter = Counter()
+	counter.set_current(7)
+	counter.step_up()
+	counter.step_up()
+	counter.step_up()
+	assert counter.get_current() == 10
+	
+	try:
+		counter.step_up()  # ValueError
+	except ValueError as e:
+		print(e)  # Достигнут максимум
+	assert counter.get_current() == 10
+	
+	counter.set_min(7)
+	counter.step_down()
+	counter.step_down()
+	counter.step_down()
+	assert counter.get_current() == 7
+	
+	try:
+		counter.step_down()  # ValueError
+	except ValueError as e:
+		print(e)  # Достигнут минимум
+	assert counter.get_current() == 7
+	print('OK')
+
