340d8e7c2b414316a0e180711c08393235a47803
MagestiUA <nikolaiq3@gmail.com>
2024-06-07 12:07:00 +0300

    Homework 15.1

---
+++ Lesson 15/Homework 15.1.py
@@ -0,0 +1,58 @@
+from typing import Tuple
+
+
+class Rectangle:
+	def __init__(self, width: int, height: int):
+		self.width = width
+		self.height = height
+
+	def get_square(self) -> int:
+		return self.width * self.height
+
+	def __eq__(self, other: object) -> bool:
+		if isinstance(other, Rectangle):
+			return self.get_square() == other.get_square()
+		return False
+
+	def __add__(self, other: 'Rectangle') -> 'Rectangle':
+		if isinstance(other, Rectangle):
+			new_area = self.get_square() + other.get_square()
+			side_1, side_2 = self.find_sides(new_area)
+			return Rectangle(side_1, side_2)
+		return NotImplemented
+
+	def __mul__(self, n: int) -> 'Rectangle':
+		if isinstance(n, int):
+			new_area = self.get_square() * n
+			side_1, side_2 = self.find_sides(new_area)
+			return Rectangle(side_1, side_2)
+		return NotImplemented
+
+	def __str__(self):
+		return f"Rectangle(width={self.width}, height={self.height}, area={self.get_square()})"
+	
+	@staticmethod
+	def find_sides(new_area: int) -> Tuple[int, int]:
+		factor_pairs = []
+		for i in range(1, int(new_area ** 0.5) + 1):
+			if new_area % i == 0:
+				factor_pairs.append((i, new_area // i))
+		factor_pairs.sort()
+		return factor_pairs[-1]
+
+
+r1 = Rectangle(2, 4)
+r2 = Rectangle(3, 6)
+r3 = Rectangle(4, 2)
+assert r1.get_square() == 8
+assert r2.get_square() == 18
+assert r1 == r3
+
+r3 = r1 + r2
+assert r3.get_square() == 26
+
+r4 = r1 * 4
+assert r4.get_square() == 32
+
+assert Rectangle(3, 6) == Rectangle(2, 9)
+print('OK')
